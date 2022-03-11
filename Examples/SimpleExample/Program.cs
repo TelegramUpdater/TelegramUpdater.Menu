84620c5154da6d56367782d1fc2551c5ac8c9f0d
immmdreza <ir310022@gmail.com>
2022-03-11 13:37:47 +0330

    Added a simple example

---
+++ Examples/SimpleExample/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Telegram.Bot.Types.Enums;
+using TelegramUpdater;
+using TelegramUpdater.Menu;
+using TelegramUpdater.Menu.Directors;
+using TelegramUpdater.UpdateContainer;
+
+var updater = new Updater("BOT_TOKEN");
+
+
+var mainMenu = new MainMenu(
+    "Simple Menu",
+    "Ready to Go? Then what are you waiting for?",
+    "SimpleMenu",
+    options: default,
+    menu: new SubMenu(
+        "Gone", "Go",
+        "Aw so you're Gone, you better come back!",
+        menu: new Back("Come")));
+
+updater.AddMenuManager("myMenu", mainMenu);
+
+updater.AddSingletonUpdateHandler(
+    UpdateType.Message,
+    async cntr =>
+    {
+        if (cntr.Updater.TryGetInlineMenuManager("myMenu", out var manager))
+        {
+            await cntr.ResponseAsync(
+                manager.MainMenu.Description,
+                replyMarkup: manager.OpeningMarkup);
+        }
+    },
+    FilterCutify.OnCommand("menu"));
+
+await updater.StartAsync();
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TelegramUpdater.Menu
-A simple menu manager.
+A simple menu manager extension for TelegramUpdater.
+
+**NOTE**: TelegramUpdater.Menu supports only **Static** menus with **inline keyboard buttons**.
+This means you can't change menu buttons or texts manually.
+
+Setup your menu and this package handles the connections between them.
+
